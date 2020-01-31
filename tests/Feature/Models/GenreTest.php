d87a13e585d930511f733f04aba20112ead2ef9f
Pedro Boudoux <pedro.boudoux@idealizza.com.br>
2020-01-31 11:13:24 -0300

    Correçao no nome das variaveis de teste de genres

--- tests/Feature/Models/GenreTest.php
+++ tests/Feature/Models/GenreTest.php
@@ -14,16 +14,16 @@ class GenreTest extends TestCase
     public function testList()
     {
         factory(Genre::class, 1)->create();
-        $categories = Genre::all();
+        $genres = Genre::all();
 
-        $genreKeys = array_keys($categories->first()->getAttributes());
+        $genreKeys = array_keys($genres->first()->getAttributes());
 
         $this->assertEqualsCanonicalizing(
             ['id', 'name', 'is_active', 'created_at', 'updated_at', 'deleted_at'],
             $genreKeys
         );
 
-        $this->assertCount(1, $categories);
+        $this->assertCount(1, $genres);
     }
 
     public function testCreateOnlyName()
